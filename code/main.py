c273ce2eb37b0ca4877c5116a14db45eacfb77c1
ReserveJudgement <150562945+ReserveJudgement@users.noreply.github.com>
2025-07-30 21:39:47 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -11,22 +11,11 @@ import os
 
 if __name__ == "__main__":
 
-    #"""
-    keys = [#os.environ.get('GOOGLE_API_KEY1'),
-            #os.environ.get('GOOGLE_API_KEY2'),
-            #os.environ.get('GOOGLE_API_KEY3'),
-            os.environ.get('GOOGLE_API_KEY4'),
-            os.environ.get('GOOGLE_API_KEY5'),
-            ]
-
-    #agent = GeminiModel("gemini-2.0-flash-lite", keys)
-    #agent = GeminiModel("gemini-2.5-flash", keys)
-    agent = GeminiModel("gemini-2.5-pro", keys)
-    #"""
-
-    """
+    key = os.environ.get('GOOGLE_API_KEY')
+    agent = GeminiModel("gemini-2.5-flash", key)
+    
+    # Generate cases
     categories = ["predator-prey",
-                  "mating-partner-selection",
                   "symbiotic-cooperation",
                   "social-foraging",
                   "habitat-selection"]
@@ -35,48 +24,60 @@ if __name__ == "__main__":
         cases = get_cases(agent, category)
         with open(category+".json", "w") as f:
             json.dump(cases, f, indent=4)
-    """
-
-    """
-    # iterate over categories
-    files = ['predator-prey.json',
-             'mating-partner-selection.json',
-             'symbiotic-cooperation.json',
-             'social-foraging.json',
-             'habitat-selection.json']
+    
+"""
+    # Get Wikipedia articles and automodel
+    files = ['predator-prey',
+             'mating-partner-selection',
+             'symbiotic-cooperation',
+             'social-foraging',
+             'habitat-selection']
     for filename in files:
         print(filename)
-        with open(filename) as f:
+        with open(filename+".json") as f:
             category = json.load(f)
-        #category = get_wiki_articles(agent, category)
-        #category = autoformalize_category(agent, category)
-        #category = expected_outcomes_category(agent, category)
-        category = validate_category_semantic(agent, category)
-        #category = update_category(agent, category)
+        #category = get_wiki_articles(agent, category) # Get Wikipedia articles
+        #category = autoformalize_category(agent, category) # Automodel cases (generate games)
+        #category = expected_outcomes_category(agent, category) # Formalize the observed outcomes in nature
+        #category = validate_category_semantic(agent, category) # Semantic validation
+        #category = update_category(agent, category) # Update when there is feedback
         if category is not None:
-            with open(filename, "w") as f:
+            with open(filename+".json", "w") as f:
                 json.dump(category, f, indent=4)
-    """
+    
 
-    #"""
+    # Game theoretic validation
     # iterate over categories
-    files = [#'predator-prey',
-             #'mating-partner-selection',
-             #'symbiotic-cooperation',
+    files = ['predator-prey',
+             'symbiotic-cooperation',
              'social-foraging',
-             #'habitat-selection'
+             'habitat-selection'
             ]
     for filename in files:
         print(filename)
         with open(filename + ".json") as f:
             category = json.load(f)
-        #category = validate_category(category)
-        #category = solve_category(category)
-        #category = get_stats_feedback(category, numpass=1)
-        validated = collect_validated_games(category)
-        print("validated: ", len(validated))
-        validated = analyze_validated_set(agent, validated)
+        #category = validate_category(category) # Syntactic validation
+        #category = solve_category(category) # Solve nash equilibria and check if outcome is in them
+        #category = get_stats_feedback(category, numpass=1) # Get statistics on success rates by pass number
+        if category is not None:
+            with open(filename + ".json", "w") as f:
+                json.dump(category, f, indent=4)
+        
+    # Analysis stage
+    files = ['predator-prey',
+         'symbiotic-cooperation',
+         'social-foraging',
+         'habitat-selection'
+        ]
+    for filename in files:
+        print(filename)
+        with open(filename + ".json") as f:
+            category = json.load(f)
+        #validated = collect_validated_games(category) # Concentrate validated game models into one file
+        #validated = analyze_validated_set(agent, validated) # Perform analysis on validated game models
         if validated is not None:
-            with open(filename + "-analysis.json", "w") as f:
+            with open(filename + ".json", "w") as f:
                 json.dump(validated, f, indent=4)
-    #"""
+        
+"""
