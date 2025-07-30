7d046f502dd2274271c4f66f2a99ea77c4cf5e5e
ReserveJudgement <150562945+ReserveJudgement@users.noreply.github.com>
2025-07-30 21:42:43 +0300

    Update llm.py

--- code/llm.py
+++ code/llm.py
@@ -1,15 +1,14 @@
 #from google import genai
-import google.generativeai as genai
+import google.generativeai as genai  # using the deprecated sdk
 import time
 import os
 import json
 
 
 class GeminiModel:
-    def __init__(self, modeltype, keys, func=None):
+    def __init__(self, modeltype, key, func=None):
         self.modeltype = modeltype
-        self.keys = keys
-        self.key = 0
+        self.key = key
         self.num_requests = 0
         self.tokens_used = 0
         self.last_time = time.time()
@@ -17,18 +16,11 @@ class GeminiModel:
                               "HARM_CATEGORY_DANGEROUS": "block_none",
                               "HARM_CATEGORY_HATE_SPEECH": "block_none",
                               "HARM_CATEGORY_SEXUALLY_EXPLICIT": "block_none"}
-        GOOGLE_API_KEY = os.environ.get(self.keys[self.key])
+        GOOGLE_API_KEY = os.environ.get(self.key)
         genai.configure(api_key=GOOGLE_API_KEY)
-        self.model = genai.GenerativeModel(self.modeltype, safety_settings=self.safety_config) #, tools=[func], tool_config={'function_calling_config':'ANY'})
-        if self.modeltype == "gemini-2.0-flash-lite":
-            self.rpm = 30
-            self.tpm = 1000000
-            self.rpd = 200
-        elif self.modeltype == "gemini-2.0-flash":
-            self.rpm = 15
-            self.tpm = 1000000
-            self.rpd = 200
-        elif self.modeltype == "gemini-2.5-flash":
+        self.model = genai.GenerativeModel(self.modeltype, safety_settings=self.safety_config) 
+        # free tier limits
+        if self.modeltype == "gemini-2.5-flash":
             self.rpm = 10
             self.tpm = 250000
             self.rpd = 250
@@ -49,15 +41,6 @@ class GeminiModel:
             time.sleep(60)
             self.tokens_used = 0
             self.last_time = time.time()
-        if self.num_requests == self.rpd:
-            print(f"Out of requests per day, switching keys to {self.key + 2}")
-            self.key += 1
-            if self.key + 1 > len(self.keys):
-                print("Out of calls")
-                exit()
-            GOOGLE_API_KEY = os.environ.get(self.keys[self.key])
-            genai.configure(api_key=GOOGLE_API_KEY)
-            self.model = genai.GenerativeModel(self.modeltype, safety_settings=self.safety_config)
         if config is not None:
             response = self.model.generate_content(prompt, generation_config=config)
         else:
